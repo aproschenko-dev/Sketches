75a12e34f7440546bfdf5943eff43eec29392f4a
Alexander Proschenko <aproschenko.dev@gmail.com>
2015-01-20 14:42:57 +0300

    sketches

---
+++ bt_commands_test/bt_commands_test.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+
+#define rxPin 2
+#define txPin 4
+
+// set up a new serial port
+SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+
+  mySerial.begin(9600);
+}
+
+void loop()
+{
+  String command = "";
+  while (mySerial.available() > 0)
+  {
+    char c = mySerial.read();
+    Serial.println(c);
+    command.concat(c);
+  }
+  
+  command.trim();
+  if (command == "")
+    return;
+
+  Serial.println("command is -> " + command);
+}
